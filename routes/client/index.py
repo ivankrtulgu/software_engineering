51bf9c37efd4d09d07b31eb0d7fd8ecfc1d4d373
ivankrtulgu <218090641+ivankrtulgu@users.noreply.github.com>
2025-11-05 00:55:58 +0300

    Исправлена регистрация

--- routes/client/index.py
+++ routes/client/index.py
@@ -1,19 +1,23 @@
 from flask_login import current_user
-from ..app import app,login_required
+from ..app import app, login_required
 from flask import render_template
-from models import Task,Session
+from models import Task, User, Session
+from sqlalchemy.orm import joinedload
 
-
-@app.route("/client",methods = ["GET"])
+@app.route("/client", methods=["GET"])
 @login_required
 def client():
     session = Session()
-    tasks = map(lambda task:task.to_dict(),
-                session.query(Task) \
-                    .filter(Task.client_id == current_user.get_id()) \
-                    .order_by(Task.date) \
-                    .all()
-                    )
-    user = current_user.client.to_dict()
-    session.close()
-    return render_template("сlient/index.html",tasks = tasks, user = user)
+    try:
+        # Загружаем current_user с клиентом сразу
+        user = session.query(User)\
+                      .options(joinedload(User.client))\
+                      .filter(User.id == current_user.get_id())\
+                      .one()
+
+        tasks = [task.to_dict() for task in user.client.tasks]
+        user_dict = user.client.to_dict()
+    finally:
+        session.close()
+
+    return render_template("client/index.html", tasks=tasks, user=user_dict)
